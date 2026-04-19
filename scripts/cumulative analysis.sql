21a6c953588480e7f9640f8c31a90ba1382c597c
olamilekabisiaka-code <olamilekabisiaka@gmail.com>
2026-04-19 21:55:28 +0100

    Create cumulative analysis.sql

---
+++ scripts/cumulative analysis.sql
@@ -0,0 +1,15 @@
+-- 📈 Running Total & Moving Average
+SELECT
+    order_date,
+    total_sales,
+    SUM(total_sales) OVER (ORDER BY order_date) AS running_total_sales,
+    AVG(avg_price) OVER (ORDER BY order_date) AS moving_avg_price
+FROM (
+    SELECT
+        DATETRUNC(MONTH, order_date) AS order_date,
+        SUM(sales_amount) AS total_sales,
+        AVG(price) AS avg_price
+    FROM dbo.fact_sales
+    WHERE order_date IS NOT NULL
+    GROUP BY DATETRUNC(MONTH, order_date)
+) t;
